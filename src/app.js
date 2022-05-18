b4b24e8335b7de364771d0fec434b7c68d971610
JonPittaluga <jon.pittaluga@gmail.com>
2022-05-18 18:16:02 +0200

    Backup commit, while I keep discovering how to  to make the algorithm work

--- src/app.js
+++ src/app.js
@@ -6,33 +6,96 @@ import { removeProperties } from './helpers/remove-properties/remove-properties.
 import { lifeStatus } from './helpers/life-status/life-status.js';
 import { filterAliveCells } from './helpers/filter-alive-cells/filter-alive-cells.js';
 
-// define the size of the matrix
-const width = 20;
-const height = 20;
+// Step 1: Define the size of the matrix
+const width = 4;
+const height = 4;
 
-// start the matrix
+// Step 2: start the matrix
 const matrix = createMatrix(width, height);
+// console.log('matrix init', matrix);
 
-// map cells to define their absolute position within the matrix -mutation-
+// Step 3: map cells to define their absolute position within the matrix -mutation-
 mapCells(matrix, width, height);
 
-// for every cell creates an array with its neighbours -mutation-
+// Step 4: for every cell creates an array with its neighbours -mutation-
 getNeighbours(matrix, width);
 
-// some cells change its isAlive property to true. Accepts the mutable matrix and a min value -mutation-
-turnOnCells(matrix, 100);
+// Step 5: some cells change its isAlive property to true. Accepts the mutable matrix, a min value and a maximum, being apercentage of the matrix length -mutation-
+// turnOnCells(matrix, 3, 0.5);
 
-// reduces unused properties -mutation-
+// TODO: Fake this to check
+
+// THE ALGORITHM IS NOT WORKING.
+matrix[0].isAlive = true;
+matrix[1].isAlive = true;
+matrix[5].isAlive = true;
+matrix[10].isAlive = true;
+
+// console.log(matrix);
+
+// Step 6: reduces unused properties -mutation-
 removeProperties(matrix);
 
 // the matrix is ready
-console.table(matrix, ['index', 'isAlive']);
+// console.table(matrix, ['index', 'isAlive']);
 
 // gameOfLife logic… mutates the matrix once
+
+// console.table(
+//   matrix.filter((cell) => !cell.isAlive),
+//   ['isAlive', 'neighbours']
+// );
+// console.table(
+//   matrix.filter((cell) => cell.isAlive),
+//   ['isAlive', 'neighbours']
+// );
+
+let arrWithOnlyAliveCells = filterAliveCells(matrix);
+
+// console.log(
+//   matrix.filter((cell) => cell.isAlive),
+//   ['isAlive']
+// );
+
+function renderBoard() {
+  let board = [];
+  let countAlive = 0;
+  board.length = matrix.length;
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (matrix[i].isAlive === true) {
+      board[i] = `${matrix[i].index}`;
+      countAlive++;
+    } else board[i] = ' ';
+  }
+  return console.log('countAlive:', countAlive, 'board:', board);
+}
+
+renderBoard();
 lifeStatus(matrix);
 
-// to check which ones are alive after the lifeStatus cicle
-const aliveCells = filterAliveCells(matrix);
+// renderBoard();
+// lifeStatus(matrix);
 
-console.table(matrix, ['index', 'isAlive']);
-console.table(aliveCells, ['index', 'isAlive']);
+// console.log('=============');
+// lifeStatus(matrix);
+// renderBoard();
+// console.log('=============');
+
+// lifeStatus(matrix);
+// renderBoard2();
+// console.log('=============');
+
+// lifeStatus(matrix);
+// renderBoard();
+// console.log('=============');
+// lifeStatus(matrix);
+// renderBoard();
+
+// setTimeout(renderBoard, 3000);
+
+// to check which ones are alive after the lifeStatus cicle
+// const aliveCells = filterAliveCells(matrix);
+
+// console.table(matrix, ['index', 'isAlive']);
+// console.table(aliveCells, ['index', 'isAlive']);
--- src/helpers/life-status/life-status.js
+++ src/helpers/life-status/life-status.js
@@ -18,115 +18,160 @@
 
 import { filterAliveCells } from '../filter-alive-cells/filter-alive-cells.js';
 
-// const matrix = [
-//   { neighbours: [1, 5, 6], index: 0, isAlive: true },
-//   { neighbours: [0, 2, 5, 6, 7], index: 1, isAlive: false },
-//   { neighbours: [1, 3, 6, 7, 8], index: 2, isAlive: true },
-//   { neighbours: [2, 4, 7, 8, 9], index: 3, isAlive: true },
-//   { neighbours: [3, 8, 9], index: 4, isAlive: true },
-//   { neighbours: [0, 1, 6, 10, 11], index: 5, isAlive: false },
-//   {
-//     neighbours: [0, 1, 2, 5, 7, 10, 11, 12],
-//     index: 6,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [1, 2, 3, 6, 8, 11, 12, 13],
-//     index: 7,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [2, 3, 4, 7, 9, 12, 13, 14],
-//     index: 8,
-//     isAlive: false,
-//   },
-//   { neighbours: [3, 4, 8, 13, 14], index: 9, isAlive: false },
-//   { neighbours: [5, 6, 11, 15, 16], index: 10, isAlive: false },
-//   {
-//     neighbours: [5, 6, 7, 10, 12, 15, 16, 17],
-//     index: 11,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [6, 7, 8, 11, 13, 16, 17, 18],
-//     index: 12,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [7, 8, 9, 12, 14, 17, 18, 19],
-//     index: 13,
-//     isAlive: false,
-//   },
-//   { neighbours: [8, 9, 13, 18, 19], index: 14, isAlive: true },
-//   {
-//     neighbours: [10, 11, 16, 20, 21],
-//     index: 15,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [10, 11, 12, 15, 17, 20, 21, 22],
-//     index: 16,
-//     isAlive: true,
-//   },
-//   {
-//     neighbours: [11, 12, 13, 16, 18, 21, 22, 23],
-//     index: 17,
-//     isAlive: true,
-//   },
-//   {
-//     neighbours: [12, 13, 14, 17, 19, 22, 23, 24],
-//     index: 18,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [13, 14, 18, 23, 24],
-//     index: 19,
-//     isAlive: false,
-//   },
-//   { neighbours: [15, 16, 21], index: 20, isAlive: false },
-//   {
-//     neighbours: [15, 16, 17, 20, 22],
-//     index: 21,
-//     isAlive: false,
-//   },
-//   {
-//     neighbours: [16, 17, 18, 21, 23],
-//     index: 22,
-//     isAlive: false,
-//   },
-//   { neighbours: [17, 18, 19, 22, 24], index: 23, isAlive: true },
-//   { neighbours: [18, 19, 23], index: 24, isAlive: false },
-// ];
-
 export function lifeStatus(matrix) {
   let aliveCells = filterAliveCells(matrix);
-  // console.log('aliveCells.length', aliveCells.length);
-  // console.table(aliveCells, ['index', 'isAlive']);
 
   for (let cell of aliveCells) {
-    const neighbours = cell.neighbours;
-    const aliveNeighbours = [];
+    // looping the smaller matrix, only alive
 
-    for (let i = 0; i < neighbours.length; i++) {
-      matrix[i].isAlive ? aliveNeighbours.push(true) : false;
+    // console.log('cell.index', cell.index);
 
-      let countAliveNeighbours = aliveNeighbours.length;
+    const neighbourIndexes = cell.neighbours;
 
-      if (countAliveNeighbours < 1) {
-        // console.log(`The cell ${cell.index} should die by solitude`);
-        matrix[cell.index].isAlive = false;
-        cell.isAlive = false;
-      } else if (countAliveNeighbours > 3) {
-        // console.log(`The cell ${cell.index} should dy by overpopulation`);
-        matrix[cell.index].isAlive = false;
-        cell.isAlive = false;
-        // cell.isAlive = true;
-      } else if (countAliveNeighbours >= 2 && countAliveNeighbours <= 3) {
-        // console.log(`The cell ${cell.index} should stay alive`);
-      }
+    let aliveNeighbours = [];
+    let locals = []; // to store neighbours of neighbours
+
+    for (let index of neighbourIndexes) {
+      matrix[index].isAlive ? aliveNeighbours.push(index) : false;
+    }
+    console.log('aliveNeighbours', aliveNeighbours);
+
+    if (aliveNeighbours.length >= 2 && aliveNeighbours.length <= 3) {
+      console.log('stay alive', matrix[cell.index].index);
+      // matrix[cell.index].isAlive = true;
+    }
+
+    if (aliveNeighbours.length < 2) {
+      console.log('death by solitude', matrix[cell.index].index);
+      // matrix[cell.index].isAlive = false;
     }
   }
-  return aliveCells.length;
+
+  console.table(aliveCells, ['isAlive', 'index']);
+  console.table(matrix, ['isAlive', 'index']);
+
+  // return aliveNeighbours.length;
+
+  // for (let cell of aliveCells) {
+  //   counterAlive++;
+
+  //   const neighbours = cell.neighbours;
+
+  //   const aliveNeighbours = []; // to count how many alive has a cell
+  //   let countAliveNeighbours = aliveNeighbours.length;
+
+  //   // console.log(`external loop: ${cell.index}`);
+  //   const locals2d = [];
+
+  //   for (let i = 0; i < neighbours.length; i++) {
+  //     // counterNeighbour++;
+  //     // console.log('counterNeighbour', counterNeighbour);
+  //     // console.log(`internal loop. Index: ${matrix[i].neighbours}`);
+
+  //     locals2d.push(matrix[i].neighbours); // pushes arrays
+
+  //     if (matrix[i].isAlive) {
+  //       aliveNeighbours.push(matrix[i]);
+  //       if (countAliveNeighbours < 2) {
+  //         // console.log(`The cell ${cell.index} should die by solitude`);
+  //         matrix[cell.index].isAlive = false;
+  //         cell.isAlive = false;
+  //       } else if (countAliveNeighbours > 3) {
+  //         // console.log(`The cell ${cell.index} should dy by overpopulation`);
+  //         matrix[cell.index].isAlive = false;
+  //         cell.isAlive = false;
+  //         // cell.isAlive = true;
+  //       } else if (countAliveNeighbours >= 2 && countAliveNeighbours <= 3) {
+  //         // console.log(`The cell ${cell.index} should stay alive`);
+  //       }
+  //     } // !matrix[i].isAlive
+
+  //     const locals1d = [].concat(...locals2d);
+  //     locals = [...new Set(locals1d)];
+  //     // console.log('locals', locals);
+  //   }
+  // }
+  // TODO: PREVIOUS VERSION DISABLED - 1623 for (let cell of aliveCells) {
+  //   counterAlive++;
+
+  //   const neighbours = cell.neighbours;
+
+  //   const aliveNeighbours = []; // to count how many alive has a cell
+  //   let countAliveNeighbours = aliveNeighbours.length;
+
+  //   // console.log(`external loop: ${cell.index}`);
+  //   const locals2d = [];
+
+  //   for (let i = 0; i < neighbours.length; i++) {
+  //     // counterNeighbour++;
+  //     // console.log('counterNeighbour', counterNeighbour);
+  //     // console.log(`internal loop. Index: ${matrix[i].neighbours}`);
+
+  //     locals2d.push(matrix[i].neighbours); // pushes arrays
+
+  //     if (matrix[i].isAlive) {
+  //       aliveNeighbours.push(matrix[i]);
+  //       if (countAliveNeighbours < 2) {
+  //         // console.log(`The cell ${cell.index} should die by solitude`);
+  //         matrix[cell.index].isAlive = false;
+  //         cell.isAlive = false;
+  //       } else if (countAliveNeighbours > 3) {
+  //         // console.log(`The cell ${cell.index} should dy by overpopulation`);
+  //         matrix[cell.index].isAlive = false;
+  //         cell.isAlive = false;
+  //         // cell.isAlive = true;
+  //       } else if (countAliveNeighbours >= 2 && countAliveNeighbours <= 3) {
+  //         // console.log(`The cell ${cell.index} should stay alive`);
+  //       }
+  //     } // !matrix[i].isAlive
+
+  //     const locals1d = [].concat(...locals2d);
+  //     locals = [...new Set(locals1d)];
+  //     // console.log('locals', locals);
+  //   }
+  // }
+
+  // for (let cell of locals) {
+  //   const neighbours = matrix[cell].neighbours;
+  //   const aliveNeighbours = []; // to count how many alive has a cell
+  //   let countAliveNeighbours = aliveNeighbours.length;
+  //   for (let i = 0; i < neighbours.length; i++) {
+  //     console.log(`locals internal loop. Index: ${matrix[i].neighbours}`);
+  //     if (matrix[i].isAlive) {
+  //       aliveNeighbours.push(matrix[i]);
+  //       if (countAliveNeighbours === 3) {
+  //         matrix[cell.index].isAlive = true;
+  //         cell.isAlive = true;
+  //       }
+  //     }
+  //   }
+  // }
+
+  // Quiero iterar sobre matrix y, si matrix[i].index está incluido en locals, guardarlo
+
+  // // FIXME:
+
+  //   let localCells = matrix.filter((cell) => locals.includes(cell.index));
+
+  //   // console.log('localCells', localCells);
+
+  //   for (let cell of localCells) {
+  //     const neighbours = cell.neighbours;
+  //     const aliveNeighbours = []; // to count how many alive has a cell
+  //     let countAliveNeighbours = aliveNeighbours.length;
+
+  //     for (let i = 0; i < neighbours.length; i++) {
+  //       // console.log(`locals internal loop. Index: ${matrix[i].neighbours}`);
+  //       if (cell.isAlive) {
+  //         aliveNeighbours.push(matrix[i]);
+  //       }
+  //     }
+
+  //     if (countAliveNeighbours === 3) {
+  //       matrix[cell.index].isAlive = true;
+  //       cell.isAlive = true;
+  //     }
+  //   }
 }
 
 // console.table(matrix, ['index', 'isAlive']);
