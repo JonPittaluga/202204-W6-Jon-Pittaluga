da6d4a3e8d14f8ae215ba8b33ca2ec9a1fa6e023
JonPittaluga <jon.pittaluga@gmail.com>
2022-05-19 10:31:11 +0200

    First html rendering version with random values. Tests pending and refactoring pending

--- src/app.js
+++ src/app.js
@@ -6,40 +6,116 @@ import { removeProperties } from './helpers/remove-properties/remove-properties.
 import { lifeStatus } from './helpers/life-status/life-status.js';
 import { filterAliveCells } from './helpers/filter-alive-cells/filter-alive-cells.js';
 import { consoleRender } from './helpers/console-render/console-render.js';
-// Step 1: Define the size of the matrix
-const width = 40; // max400
-const height = 30; // max300
 
-// Step 2: start the matrix
+const width = 150;
+const height = 100;
+const matrixStart = { minAlive: 5, maxAlive: 30 };
+const duration = 10000; // miliseconds
+const renderingSpeed = 100; // miliseconds
+
 const matrix = createMatrix(width, height);
 
-// Step 3: map cells to define their absolute position within the matrix -mutation-
-mapCells(matrix, width, height);
+const renderCell = (cell) => {
+  const status = cell.isAlive ? 'isAlive' : 'isDead';
+  return `<div id="${cell.index}" class="cell cell--${status}"></div>`;
+};
 
-// Step 4: for every cell creates an array with its neighbours -mutation-
-getNeighbours(matrix, width);
+const renderMatrix = () => {
+  const gridColumns = `grid-template-columns: repeat(${width}, 1fr)`;
+  let html = `<div class="matrix" style="${gridColumns}">`;
+  const getCellsRendered = matrix.map((cell) => renderCell(cell));
+  html += getCellsRendered.join('');
+  html += '</div>';
+  // return 'hOLA';
+  return html;
+};
 
-// Step 5: some cells change its isAlive property to true. Accepts the mutable matrix, a min value and a maximum, being apercentage of the matrix length -mutation-
-turnOnCells(matrix, 10, 0.15);
+function startGameOfLife(durationMili) {
+  // Step 1: Define the size of the matrix
+  // const width = 40; // max400
+  // const height = 30; // max300
 
-// Hardcoded checks
-// matrix[0].isAlive = true;
-// matrix[1].isAlive = true;
+  // Step 2: start the matrix
+  // Step 3: map cells to define their absolute position within the matrix -mutation-
+  mapCells(matrix, width, height);
 
-// Step 6: reduces unused properties -mutation-
-removeProperties(matrix);
+  // Step 4: for every cell creates an array with its neighbours -mutation-
+  getNeighbours(matrix, width);
 
-// the matrix is ready, now let's render it in the console
+  // Step 5: some cells change its isAlive property to true. Accepts the mutable matrix, a min value and a maximum, being apercentage of the matrix length -mutation-
+  turnOnCells(matrix, matrixStart.minAlive, matrixStart.maxAlive);
 
-function repeatCode() {
-  let aliveCells = filterAliveCells(matrix);
-  consoleRender(matrix);
-  lifeStatus(matrix);
-  if (aliveCells.length === 0) {
-    console.log('NO ALIVE CELLS');
-    return;
+  // Hardcoded checks
+  // matrix[0].isAlive = true;
+  // matrix[1].isAlive = true;
+
+  // Step 6: reduces unused properties -mutation-
+  removeProperties(matrix);
+
+  // the matrix is ready, now let's render it in the console
+
+  let lifeExists = true;
+  const ecosystemIsOver = () => {
+    lifeExists = false;
+  };
+
+  function repeatCode() {
+    renderMatrix();
+    let aliveCells = filterAliveCells(matrix);
+    consoleRender(matrix);
+    lifeStatus(matrix);
+    if (aliveCells.length === 0) {
+      ecosystemIsOver();
+      console.log('lifeExists', lifeExists);
+    }
+    return aliveCells.length;
   }
+
+  let intervalID = setInterval(repeatCode, renderingSpeed); // 50ms in console works
+  setTimeout(clearInterval, durationMili, intervalID);
+  // const headerInfo__intervalsValue = document.querySelector(
+  //   '.header-info__intervals--value'
+  // );
+  // headerInfo__intervalsValue.innerHTML = intervalID;
 }
 
-const intervalID = setInterval(repeatCode, 150);
-setTimeout(clearInterval, 15000, intervalID);
+// TODO: IMPROVE THE WAY ALL STARTS
+const fiatMatrix = () => {
+  startGameOfLife(duration); // minAlive, maxAlive, duration
+  console.log('exitOption');
+};
+
+fiatMatrix();
+
+const btn = document.querySelector('.start-btn');
+
+const headerInfo = document.querySelector('.header-info');
+const headerInfo__width = document.querySelector('.header-info__width');
+const headerInfo__widthValue = document.querySelector(
+  '.header-info__width--value'
+);
+const headerInfo__height = document.querySelector('.header-info__height');
+const headerInfo__heightValue = document.querySelector(
+  '.header-info__height--value'
+);
+const headerInfo__cellsValue = document.querySelector(
+  '.header-info__cells--value'
+);
+const headerInfo__intervalsValue = document.querySelector(
+  '.header-info__intervals--value'
+);
+const headerInfo__renderingValue = document.querySelector(
+  '.header-info__rendering--value'
+);
+
+const matrixContainer = document.querySelector('.container');
+const matrixContainerId = document.querySelector('#matrix-container');
+
+matrixContainerId.innerHTML = renderMatrix();
+// console.log(matrixContainerId.innerHTML);
+
+headerInfo__widthValue.innerHTML = width;
+headerInfo__heightValue.innerHTML = height;
+headerInfo__cellsValue.innerHTML = width * height;
+headerInfo__intervalsValue.innerHTML = duration / 1000 + 's';
+headerInfo__renderingValue.innerHTML = renderingSpeed + 'ms';
--- src/helpers/console-render/console-render.js
+++ src/helpers/console-render/console-render.js
@@ -4,14 +4,29 @@ export function consoleRender(matrix) {
   board.length = matrix.length;
 
   for (let i = 0; i < matrix.length; i++) {
-    if (matrix[i].isAlive === true) {
-      board[i] = `<·>`;
+    let target = document.getElementById(i); // to render cells
+
+    if (matrix[i].isAlive) {
+      // board[i] = `<·>`;
+      board[i] = 99;
       countAlive++;
-    } else board[i] = '  ';
+      target.style = 'background-color: rgb(125, 226, 47)';
+    }
+
+    if (!matrix[i].isAlive) {
+      board[i] = ' ';
+      target.style = 'background-color: rgb(39,41,32)';
+    }
   }
 
   if (countAlive) {
-    console.log('countAlive:', countAlive, board);
+    // console.log('countAlive:', countAlive, board);
+    // console.log(
+    //   `alive cells: ${countAlive} / ${(
+    //     (countAlive * 100) /
+    //     board.length
+    //   ).toFixed(3)}%`
+    // );
   }
 
   return board;
